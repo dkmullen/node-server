58607e1a4d06bd25a2621b444a50573bd62e4b5b
Dennis Mullen <dkmullen@gmail.com>
2017-11-11 11:07:05 -0500

    Add some middleware for a server long and for maintenance mode

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 /*jshint esversion: 6 */
 
 const express = require('express'),
-  hbs = require('hbs');
+  hbs = require('hbs'),
+  fs = require('fs');
 
 const app = express();
 
@@ -10,10 +11,33 @@ app.set('view engine', 'hbs');
 // __dirname gives the absolute path name to the present file
 // register partial templates to be used as parts of pages
 hbs.registerPartials(__dirname + '/views/partials');
+
+// This is middleware to make and store a server activity log
+// app.use is the syntax for registering middleware
+// next tells express that the middleware is done, time to move on - don't forget to call it!
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// This is middleware for maintenance mode, display maintenance page on all routes
+// on this one, we don't call next because we want this one to block the rest
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+// Middleware that tells express to access /public/help.html as localhost:3000/help.html
 // express.static is built-in middleware to serve static files
-// this command allows me to access /public/help.html as localhost:3000/help.html
 app.use(express.static(__dirname + '/public'));
 
+// Register a Handlebars helper function, called in templates
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
@@ -24,6 +48,7 @@ hbs.registerHelper('screamIt', (text) => {
 
 // Set up a handler for GET - always these two args
 app.get('/', (req, res) => { // to any req on /
+  // Can send html, an object, or call a render on a template
   // res.send('<h1>Hello Express!</h1>'); // send this response
   res.render('home.hbs', {
     pageTitle: 'Home Page',
@@ -38,6 +63,7 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/bad', (req, res) => {
+  // In this case, the response is sent in json
   res.send({
     problem: 'You got knocked out.',
     message: 'This is the error',
